fb85f4d6e8a3a1bc681187d4ef3badb71e52e1fe
floriggi <iggiotti.florian@neuf.fr>
2022-04-28 17:07:14 +0200

    Ajout exo9 - TD4.

---
+++ TD4/exo9a.c
@@ -0,0 +1,56 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define MAX 100
+
+void erreur(const char *message) {
+    printf("Erreur durant : %s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+void lireMessage(char tampon[]) {
+    printf("Saisir un message à envoyer :\n");
+    fgets(tampon, MAX, stdin);
+}
+
+void creerFichier(const char *nom) {
+    if (mkfifo(nom, 0644) != 0) {
+        erreur("mkfifo");
+    }
+}
+
+int ouvrirFichier(char *nom) {
+    int fd;
+    if ((fd = open(nom, O_WRONLY)) < 0) {
+        erreur("ouverture fichier");
+    }
+
+    return fd;
+}
+
+void fermerFichier(int fichier) {
+    close(fichier);
+}
+
+int main(int argc, char const *argv[]) {
+    int fichier;
+    char *nomFifo = "/tmp/fifo";
+    char buffer[MAX];
+
+    printf("Exercice 9 - 1\n");
+
+    creerFichier(nomFifo);
+    fichier = ouvrirFichier(nomFifo);
+
+    lireMessage(buffer);
+
+    write(fichier, buffer, strlen(buffer) + 1);
+
+    fermerFichier(fichier);
+
+    return EXIT_SUCCESS;
+}
---
+++ TD4/exo9b.c
@@ -0,0 +1,49 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX 100
+
+void erreur(const char *message) {
+    printf("Erreur durant : %s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+int ouvrirFichier(char *nom) {
+    int fd;
+    if ((fd = open(nom, O_RDONLY)) < 0) {
+        erreur("ouverture fichier");
+    }
+
+    return fd;
+}
+
+void fermerFichier(int fichier) {
+    close(fichier);
+}
+
+void effacerFichier(char const *nomFichier) {
+    if (unlink(nomFichier) != 0) {
+        erreur("effacer fichier");
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int fichier;
+    char *nomFifo = "/tmp/fifo";
+    char buffer[MAX];
+
+    printf("Exercice 9 - 2\n");
+
+    fichier = ouvrirFichier(nomFifo);
+    read(fichier, buffer, MAX);
+
+    printf("Message recu : %s\n", buffer);
+
+    fermerFichier(fichier);
+    effacerFichier(nomFifo);
+
+    return EXIT_SUCCESS;
+}
